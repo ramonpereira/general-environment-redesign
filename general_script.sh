0ef75700d170005c2b6fad7b954e433e32e74866
apozanco <alberto.pozanco@gmail.com>
2023-08-10 19:40:04 +0200

    General script

---
+++ general_script.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+echo 'Running run_bash_3.sh'
+nohup ./run_bash_3.sh &
+wait
+echo 'Running run_bash_29.sh'
+nohup ./run_bash_29.sh &
+wait
+echo 'Running run_bash_39.sh'
+nohup ./run_bash_39.sh &
+wait
+echo 'Running run_bash_7.sh'
+nohup ./run_bash_7.sh &
+wait
+echo 'Running run_bash_19.sh'
+nohup ./run_bash_19.sh &
+wait
+echo 'Running run_bash_6.sh'
+nohup ./run_bash_6.sh &
+wait
+echo 'Running run_bash_18.sh'
+nohup ./run_bash_18.sh &
+wait
+echo 'Running run_bash_2.sh'
+nohup ./run_bash_2.sh &
+wait
+echo 'Running run_bash_28.sh'
+nohup ./run_bash_28.sh &
+wait
+echo 'Running run_bash_38.sh'
+nohup ./run_bash_38.sh &
+wait
+echo 'Running run_bash_46.sh'
+nohup ./run_bash_46.sh &
+wait
+echo 'Running run_bash_17.sh'
+nohup ./run_bash_17.sh &
+wait
+echo 'Running run_bash_23.sh'
+nohup ./run_bash_23.sh &
+wait
+echo 'Running run_bash_9.sh'
+nohup ./run_bash_9.sh &
+wait
+echo 'Running run_bash_33.sh'
+nohup ./run_bash_33.sh &
+wait
+echo 'Running run_bash_27.sh'
+nohup ./run_bash_27.sh &
+wait
+echo 'Running run_bash_13.sh'
+nohup ./run_bash_13.sh &
+wait
+echo 'Running run_bash_42.sh'
+nohup ./run_bash_42.sh &
+wait
+echo 'Running run_bash_37.sh'
+nohup ./run_bash_37.sh &
+wait
+echo 'Running run_bash_26.sh'
+nohup ./run_bash_26.sh &
+wait
+echo 'Running run_bash_12.sh'
+nohup ./run_bash_12.sh &
+wait
+echo 'Running run_bash_43.sh'
+nohup ./run_bash_43.sh &
+wait
+echo 'Running run_bash_36.sh'
+nohup ./run_bash_36.sh &
+wait
+echo 'Running run_bash_47.sh'
+nohup ./run_bash_47.sh &
+wait
+echo 'Running run_bash_16.sh'
+nohup ./run_bash_16.sh &
+wait
+echo 'Running run_bash_22.sh'
+nohup ./run_bash_22.sh &
+wait
+echo 'Running run_bash_8.sh'
+nohup ./run_bash_8.sh &
+wait
+echo 'Running run_bash_32.sh'
+nohup ./run_bash_32.sh &
+wait
+echo 'Running run_bash_25.sh'
+nohup ./run_bash_25.sh &
+wait
+echo 'Running run_bash_40.sh'
+nohup ./run_bash_40.sh &
+wait
+echo 'Running run_bash_11.sh'
+nohup ./run_bash_11.sh &
+wait
+echo 'Running run_bash_35.sh'
+nohup ./run_bash_35.sh &
+wait
+echo 'Running run_bash_15.sh'
+nohup ./run_bash_15.sh &
+wait
+echo 'Running run_bash_44.sh'
+nohup ./run_bash_44.sh &
+wait
+echo 'Running run_bash_21.sh'
+nohup ./run_bash_21.sh &
+wait
+echo 'Running run_bash_31.sh'
+nohup ./run_bash_31.sh &
+wait
+echo 'Running run_bash_14.sh'
+nohup ./run_bash_14.sh &
+wait
+echo 'Running run_bash_45.sh'
+nohup ./run_bash_45.sh &
+wait
+echo 'Running run_bash_20.sh'
+nohup ./run_bash_20.sh &
+wait
+echo 'Running run_bash_30.sh'
+nohup ./run_bash_30.sh &
+wait
+echo 'Running run_bash_24.sh'
+nohup ./run_bash_24.sh &
+wait
+echo 'Running run_bash_41.sh'
+nohup ./run_bash_41.sh &
+wait
+echo 'Running run_bash_10.sh'
+nohup ./run_bash_10.sh &
+wait
+echo 'Running run_bash_34.sh'
+nohup ./run_bash_34.sh &
+wait
+echo 'Running run_bash_5.sh'
+nohup ./run_bash_5.sh &
+wait
+echo 'Running run_bash_1.sh'
+nohup ./run_bash_1.sh &
+wait
+echo 'Running run_bash_0.sh'
+nohup ./run_bash_0.sh &
+wait
+echo 'Running run_bash_4.sh'
+nohup ./run_bash_4.sh &
+wait
